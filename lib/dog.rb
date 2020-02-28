9c7ac74d5ac3049062f5e3e6b1373ec7b911fc7f
Christian Fredericksen <christian.fredericksen@yahoo.com>
2020-02-28 22:22:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,16 +8,16 @@ class Dog
   end
   
   
-  def self.create_table
-    sql = <<-SQL
-    CREATE TABLE IF NOT EXISTS dogs (
-      id INTEGER PRIMARY KEY 
-      name TEXT 
-      breed TEXT 
-      )
-      SQL
+  # def self.create_table
+  #   sql = <<-SQL
+  #   CREATE TABLE IF NOT EXISTS dogs (
+  #     id INTEGER PRIMARY KEY 
+  #     name TEXT 
+  #     breed TEXT 
+  #     )
+  #     SQL
       
-      DB[:conn].execute(sql)
-    end
+  #     DB[:conn].execute(sql)
+  #   end
   
 end
